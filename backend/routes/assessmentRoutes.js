e18b8cc5aa61d806b617659d4a85d75f6a9c3384
ivisrakshitha <rakshitha@ivislabs.com>
2024-12-11 15:08:15 +0530

    11DEC20241508

--- backend/routes/assessmentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createAssessment, getAssessments } = require('../controllers/assessmentController');
-
-router.post('/create', createAssessment);
-router.get('/list', getAssessments);
-
-module.exports = router;
